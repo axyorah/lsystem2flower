241a13e358ac0ae065e12e9abb6c6c232832f1b4
axyorah <elvira.bozileva@gmail.com>
2020-11-28 12:44:13 +0100

    mod: moved LSystem class to modules/lsystem/lsystem.js

--- main.js
+++ main.js
@@ -1,88 +1,9 @@
 // GLOBALS
 var canvas = document.getElementById("canvas");
-const context = canvas.getContext('2d');
+const context = canvas.getContext("2d");
 var turtle = new TURTLE("canvas");
+let lsys = new LSystem(turtle, context);
 
-class LSystem {
-    constructor(){
-        this.reset();
-    }
-
-    dist = 10; // [pxl] length of `F` segment
-    angle = 15; // [deg] angle between child- and parent-branch directions
-    states = ['X'];
-    rules = {
-        'X': 'F[-X][X]F[-X]+FX',
-        'F': 'FF',
-        '[': '[',
-        ']': ']',
-        '+': '+',
-        '-': '-',
-    };
-
-    setRules = function() {
-        //TODO
-        console.log(this.rules);
-    }
-
-    setDist = function() {
-        //TODO
-        console.log(this.dist);
-    }
-
-    setAngle = function() {
-        //TODO
-        console.log(this.angle);
-    }
-
-    reset = function(resetState=true) {
-        if (resetState) {
-            this.states = ['X'];
-        }
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        turtle.moveTo(canvas.width/2,Math.floor(canvas.height * 0.95));
-        turtle.dir = - Math.PI / 2;
-        turtle.stroke();
-    }
-
-    updateState = function(iters=1) {
-        let state0;
-        let state;
-        for (let i = 0; i < iters; i++) {
-            state0 = this.states[this.states.length-1];
-            state = "";
-            for (let sym of state0) {
-                state += this.rules[sym];
-            }
-            this.states.push(state);
-        }
-        return state;
-    }
-
-    draw = function() {
-        let pos_stack = [];
-        let dir_stack = [];
-        for (let sym of this.states[this.states.length-1]) {
-            if (sym === 'F') turtle.forward(this.dist)
-            else if (sym === '+') turtle.right(this.angle)
-            else if (sym === '-') turtle.left(this.angle)
-            else if (sym === '[') {
-                pos_stack.push([turtle.x, turtle.y]) 
-                dir_stack.push(turtle.dir)
-            }
-            else if (sym === ']') {
-                turtle.stroke();
-                turtle.penup();
-                turtle.moveTo(...pos_stack[pos_stack.length-1]);
-                turtle.dir = dir_stack[dir_stack.length-1];
-                pos_stack.pop();
-                dir_stack.pop();
-                turtle.pendown();
-            }
-        }
-        turtle.stroke();
-    }
-};
 
 const growOneStep = function() {
     lsys.reset(false);
@@ -100,5 +21,3 @@ const reset = function() {
     lsys.reset();
     lsys.draw();
 }
-
-let lsys = new LSystem();
