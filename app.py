f99e4b06a147b41a6cbe6039586ba956ba5dd1ff
axyorah <elvira.bozileva@gmail.com>
2021-06-08 15:38:31 +0200

    mod: mild refactor/cleanup

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 
-import flask
 from flask import Flask, request, render_template
-
-import numpy as np
 import os
-
-import tensorflow as tf
-from tensorflow.keras.models import load_model
 
 from utils.imgutils import b64image2array, canvas2edges, edges2generated, \
     generated2postprocessed, postprocessed2encoded
 
 from keras_model.get_model import Generator
 
-KERAS_WEIGHTS = "./keras_model/weights_50nybn/"#"./keras_model/generator_50colab_nybn.h5"
+KERAS_WEIGHTS = "./keras_model/weights_50nybn/"
 SECRET_KEY = os.urandom(16)
 
 app = Flask(__name__)    
+
+def load_generator(weight_dir):
+    generator = Generator()
+    for i,layer in enumerate(generator.layers):
+        if layer.get_weights():
+            layer_name = os.path.join(weight_dir, f"layer_{i}.h5")
+            layer.load_weights(layer_name)
+    return generator
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -54,10 +56,6 @@ def flowerify():
 if __name__ == "__main__":
     Flask.secret_key = SECRET_KEY
     
-    generator = Generator()
-    for i,layer in enumerate(generator.layers):
-        if layer.get_weights():
-            layer_name = os.path.join(KERAS_WEIGHTS, f"layer_{i}.h5")
-            layer.load_weights(layer_name)
+    generator = load_generator(KERAS_WEIGHTS)
 
     app.run(host="0.0.0.0")
